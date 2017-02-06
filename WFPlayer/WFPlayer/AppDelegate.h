7e0539fc4235f472d4be614b9ac70a2acd4d7b4b
babywolf <591138406@qq.com>
2017-02-06 14:41:32 +0800

    init project

---
+++ WFPlayer/WFPlayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WFPlayer
+//
+//  Created by babywolf on 17/2/6.
+//  Copyright © 2017年 babywolf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
